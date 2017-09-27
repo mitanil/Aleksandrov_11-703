9ff5ca886927f81e2ea07e51115760d690043ed1
mitanil <mitanil@mail.ru>
2017-09-27 15:50:42 +0300

    remove for and add for

--- LessonFor/src/ru/itis/Main.java
+++ LessonFor/src/ru/itis/Main.java
@@ -6,9 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 	    Scanner s  = new Scanner(System.in);
+        System.out.print("Введите два числа: ");
         int a = s.nextInt(), b = s.nextInt();
-        for(int i = a; i <= b; i++){
-            System.out.print(i + " ");
-        }
+//        for(int i = a; i <= b; i++){
+//            System.out.print(i + " ");
+//        }
+        System.out.print("Числа между заданными числами: ");
+        for(;a<=b; a++) System.out.print(a + " ");
     }
 }
